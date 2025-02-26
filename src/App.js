28b85d6300c8c50012ffffe7ca3beb2275517558
mrrglgl <ntfims@gmail.com>
2025-02-26 19:34:26 +0700

    Ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp;
+
 
 function App() {
 
@@ -8,14 +8,12 @@ function App() {
     tg.ready()
   }, [])
 
-  const onClose = () => {
-    tg.close()
-  }
+
 
   return (
     <div className="App">
       HUI
-      <button onClick={onClose}>Закрыть</button>
+      <button>Закрыть</button>
     </div>
   );
 }
